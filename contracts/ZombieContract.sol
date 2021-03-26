2a4cd196c4827021ddc85f45ba4b53aae21e52d9
Benoit SAGOLS <benoit.sagols@gmail.com>
2021-03-26 13:44:16 +0800

    L01C05

--- contracts/ZombieContract.sol
+++ contracts/ZombieContract.sol
@@ -3,4 +3,9 @@ pragma solidity ^0.5.8;
 contract ZombieFactory {
   uint256 internal dnaDigits = 16;
   uint256 internal dnaModulus = 10**dnaDigits;
+
+  struct Zombie {
+    string name;
+    uint256 dna;
+  }
 }
